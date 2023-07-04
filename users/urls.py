93a14365ae8aa7a00a087d0cd684d5dc8695fd97
Nikolai Popov <nt.popov@prof-itgroup.ru>
2023-07-05 00:45:28 +0300

    register

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 
+from users.views import register
+
 urlpatterns = [
-    path('', include('django.contrib.auth.urls'))
+    path('', include('django.contrib.auth.urls')),
+    path('register/', register.as_view(), name = 'register')
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+class register(View):
+    template_name = 'registration/register.html'
+    def get(self, request):
+        context = {
+            'form':UserCreationForm()
+        }
+        return render(request, self.template_name, context)
