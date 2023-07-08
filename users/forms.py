509403d2b735936b750dff25b067c4bc54e31648
Nikolai Popov <nikolaitpopov@gmail.com>
2023-07-08 16:05:10 +0300

    final changes

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,14 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import forms
 
 User = get_user_model()
 class UserCreationForm(UserCreationForm):
+    class PasswordResetForm(forms.Form):
+        email = forms.EmailField(
+            label=_("Email"),
+            max_length=254,
+            widget=forms.EmailInput(attrs={"autocomplete": "email"}),
+        )
     class Meta(UserCreationForm.Meta):
         model = User
