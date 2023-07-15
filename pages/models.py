a73ee6c06aedc6160814a79d1db0aac4cdc4cf1b
Nikolai Popov <nikolaitpopov@gmail.com>
2023-07-15 17:54:13 +0300

    pages users connection added

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+class page(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, default=0)
+
+    def __str__(self):
+        return self.title
