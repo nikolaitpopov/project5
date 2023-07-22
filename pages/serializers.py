89ccaa9aa0a085e32e025cbe805c32652ebb2c39
Nikolai Popov <nikolaitpopov@gmail.com>
2023-07-22 23:48:10 +0300

    rest api attempt

--- pages/serializers.py
+++ pages/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 
-from pages.models import page
+from pages.models import page, paragraph
 
 
 class pagesSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = page
         fields = ['title', 'content']
+
+class paragraphSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = paragraph
+        fields = ['content', 'parent', 'page', 'id']
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from pages.models import page
+from pages.models import page, paragraph
 from rest_framework import viewsets
 from pages.serializers import pagesSerializer
 # Create your views here.
 class Pages_View (viewsets.ModelViewSet):
     queryset = page.objects.all()
+    serializer_class = pagesSerializer
+
+class Paragraphs_View (viewsets.ModelViewSet):
+    queryset = paragraph.objects.all()
     serializer_class = pagesSerializer
