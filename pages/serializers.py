25f6205b8177bef2dd3b6694996f4bebf8ee1b68
Nikolai Popov <nikolaitpopov@gmail.com>
2023-07-27 22:49:22 +0300

    rest succeeded2 +id not url

--- pages/serializers.py
+++ pages/serializers.py
@@ -3,12 +3,12 @@ from rest_framework import serializers
 from pages.models import page, paragraph
 
 
-class pagesSerializer(serializers.HyperlinkedModelSerializer):
+class pagesSerializer(serializers.ModelSerializer):
     class Meta:
         model = page
         fields = ['title', 'content']
 
-class paragraphSerializer(serializers.HyperlinkedModelSerializer):
+class paragraphSerializer(serializers.ModelSerializer):
     class Meta:
         model = paragraph
         fields = ['content', 'parent', 'page', 'id']
